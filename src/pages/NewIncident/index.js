9b6485664766a405c957b4e70591f064a77cfe13
GabrielDevCSharp <gabriel0211oliveira@outlook.com>
2020-04-11 18:01:23 -0300

    frontend Projeto ONG

---
+++ src/pages/NewIncident/index.js
@@ -0,0 +1,79 @@
+import React, {  useState }from 'react';
+import {Link, useHistory } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncidet() {
+  const[title, settitle] = useState('');
+  const[description, setdescription] =('');
+  const[value, setvalue] = ('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+      try {
+        await api.post('incidents', data, {
+          header: {
+            authorization: ongId,
+          }
+        })
+        history.push('/profile');
+      }
+      catch (err) {
+        alert('Erro ao cadastrar caso, tente novamente! ');
+      }
+
+  }
+  return(
+    <div className="new-incident-container ">
+      <div className="content" >
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link className= "back-link" to="/profile">
+            <FiArrowLeft size={16} color ='#e02041' />
+              Voltar para home
+          </Link>
+          <form onSubmit={handleNewIncident}>
+            <input 
+              placeholder="Titulo do caso"
+              value={title}
+              onChange={e =>  settitle(e.target.value)}
+            />
+
+            <textarea 
+              placeholder="Descrição"
+              value={description}
+              onChange={e =>  setdescription(e.target.value)}
+            />
+            
+            <input 
+              placeholder="Valor em reais"
+              value={value}
+              onChange={e =>  setvalue(e.target.value)}
+            />
+             <button className="button" type="submit">Cadastrar</button>
+
+          </form>
+        </section>
+      </div>
+    </div>
+  )
+}
